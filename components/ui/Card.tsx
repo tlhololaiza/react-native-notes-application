694379855255f730168056c300a9331c217f53d4
tlhololaiza <tshwanetlholo7@gmail.com>
2025-11-19 16:50:49 +0200

    card component created

---
+++ components/ui/Card.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from 'react';
+import { StyleSheet, View, ViewStyle } from 'react-native';
+import { Colors } from '../../constants/Colors';
+
+interface CardProps {
+  children: ReactNode;
+  style?: ViewStyle;
+}
+
+export const Card = ({ children, style }: CardProps) => {
+  return (
+    <View style={[styles.card, style]}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: Colors.card,
+    borderRadius: 16,
+    padding: 16,
+    marginBottom: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+  },
+});
