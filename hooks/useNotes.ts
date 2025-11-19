7bdd7681f9748c5ef2221be6eb679fa1de721998
tlhololaiza <tshwanetlholo7@gmail.com>
2025-11-19 17:36:22 +0200

    notes hooks created

---
+++ hooks/useNotes.ts
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { NotesContext } from '../context/NotesContext';
+
+export const useNotes = () => {
+  const context = useContext(NotesContext);
+  
+  if (context === undefined) {
+    throw new Error('useNotes must be used within a NotesProvider');
+  }
+  
+  return context;
+};
